17ec06a3f129b3334c7019a21b8314f232d14304
Ricky Espinoza <rickyjespinoza@gmail.com>
2023-07-02 21:50:19 -0700

    added some css to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,83 @@
-import React from 'react'
-import '../styles/Home.css'
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+
+import mockCats from './mockCats';
+
 
 const Home = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === mockCats.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? mockCats.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = mockCats.map((item) => {
+    return (
+      <CarouselItem
+        className="custom-tag"
+        tag="div"
+        key={item.id}
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+      >
+        <CarouselCaption
+          className="text-danger"
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
   return (
-    <div className="home" >
-      <h1>This is a app for all Thundercats.</h1>
+    <div>
+      <style>
+        {`.custom-tag {
+              max-width: 100%;
+              height: 700px;
+              background: black;
+            }`}
+      </style>
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          item={mockCats}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
